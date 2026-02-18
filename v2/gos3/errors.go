530d6b8aa023780f59672293e4e1f79e63de430f
Gilberto (Daniel) <43019915+ggarcia209@users.noreply.github.com>
2026-02-18 14:11:28 -0800

    Fix presigned url checksums (#9)
    * fix sha256 checksum
    
    * set sha256 checksum value in file metadata

--- v2/gos3/errors.go
+++ v2/gos3/errors.go
@@ -1,6 +1,7 @@
 package gos3
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/ggarcia209/go-aws-v2/v2/goaws"
@@ -13,5 +14,15 @@ type ItemNotFoundError struct {
 func NewItemNotFoundError(item string) error {
 	return &ItemNotFoundError{
 		goaws.NewClientError(fmt.Errorf("item not found: %s", item)),
+	}
+}
+
+type MissingChecksumError struct {
+	*goaws.InternalError
+}
+
+func NewMissingChecksumError() error {
+	return &MissingChecksumError{
+		goaws.NewInternalError(errors.New("missing checksum")),
 	}
 }
